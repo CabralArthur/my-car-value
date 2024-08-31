5055b1607ecd959eba049e7b55f01897f5e20625
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2024-08-31 15:35:53 -0300

    ft: inserting find, create and update routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-    constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    @Post('/signup')
-    async createUser(@Body() body: CreateUserDto) {
-        await this.usersService.create(body.email, body.password);
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    await this.usersService.create(body.email, body.password);
 
-        return true;
-    }
+    return true;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,39 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-    create(email: string, password: string) {
-        const user = this.repo.create({ email, password });
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
 
-        return this.repo.save(user);
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  update(id: number, attrs: Partial<User>) {
+    const user = this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new Error('User not found');
     }
+
+    return this.repo.update(id, attrs);
+  }
+
+  remove(id: number) {
+    const user = this.repo.findOneBy({ id });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return this.repo.delete(id);
+  }
 }
